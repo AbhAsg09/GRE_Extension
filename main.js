d8db06632adf9baf02e67f2cbc2285dde8e6ed04
Tanmay000009 <tanmayvyas09@gmail.com>
2021-09-06 13:25:12 +0530

    Word saved after search

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-
+let lastWord = localStorage.getItem('lastWord');
+console.log(lastWord);
+lastWord = JSON.parse(lastWord);
+console.log(lastWord);
+if (lastWord) {
+    document.querySelector('.gre-word').innerHTML=lastWord[0].word;
+    document.querySelector('.gre-pos').innerHTML=lastWord[0].pos;
+    document.querySelector('.gre-desc').innerHTML=lastWord[0].desc;
+    localStorage.setItem('lastWord',null);
+} else {
     chrome.runtime.sendMessage({name: "fetchWord"}, (response) => {
         document.querySelector('.gre-word').innerHTML=response.word;
         document.querySelector('.gre-pos').innerHTML=response.pos;
         document.querySelector('.gre-desc').innerHTML=response.desc;
     });
-
+}
 
 document.querySelector('button.next').addEventListener('click', async function() {
     await chrome.runtime.sendMessage({name: "fetchWord"}, (response) => {
@@ -29,9 +38,26 @@ document.querySelector('.saveButton').addEventListener('click', function (event)
 });
 
 document.querySelector('.form-check-input').addEventListener('click', function (event) {
-    
+    let lastWord = {"word" : document.querySelector('.gre-word').innerHTML,
+    "pos":document.querySelector('.gre-pos').innerHTML,
+    "desc":document.querySelector('.gre-desc').innerHTML };
+    lastWord = [lastWord];
+    lastWord = JSON.stringify(lastWord);
+    localStorage.setItem('lastWord', lastWord);
 
     console.log("Hi");
     newURL= "https://www.google.com/search?q=" + document.querySelector('.gre-word').innerHTML ; 
     chrome.tabs.create({ url: newURL });
 });
+
+/*
+document.querySelector('button.savedItems').addEventListener('click', function () {
+    chrome.runtime.sendMessage({
+        name: "fetchSaved",
+    }, (response) => {
+        document.querySelector('h1').innerHTML=response.word;
+        document.querySelector('h4').innerHTML=response.pos;
+        document.querySelector('p').innerHTML=response.desc;
+    })
+});
+*/
